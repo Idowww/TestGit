1aaf8db20a4acd39dd4a667985098d0c1a67a9f7
jay <1297942695@qq.com>
2020-05-13 18:19:34 +0800

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	src/main/java/Helloworld.java

--- src/main/java/Helloworld.java
+++ src/main/java/Helloworld.java
@@ -2,7 +2,7 @@ public class Helloworld {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello123123234...！");
+        System.out.println("Hello this is the first git project!");
     }
 
 }
